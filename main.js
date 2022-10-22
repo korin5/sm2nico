80e70be7ac340229589d1e9055f001c83e448bda
Liang <1878464932@qq.com>
2022-10-22 17:24:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,12 +13,40 @@
 (function() {
     var smNum;
 
-    
+
     setTimeout(()=>{
         smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
         document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
         document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
         document.querySelector(".smLink").innerHTML = "跳转" + smNum;
     }, 2000);
+
+    setTimeout(()=>{
+        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
+        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
+        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
+        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
+    }, 5000);
+
+    setTimeout(()=>{
+        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
+        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
+        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
+        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
+    }, 6000);
+
+    setTimeout(()=>{
+        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
+        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
+        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
+        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
+    }, 7000);
+
+    setTimeout(()=>{
+        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
+        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
+        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
+        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
+    }, 8000);
 
 })();
