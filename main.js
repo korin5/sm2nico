6724bc1ec03f8a4cb6b7ce90068db86f3b9359d6
Liang <1878464932@qq.com>
2022-10-23 10:20:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,65 +2,41 @@
 // @name         sm号跳转
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @include      *://www.bilibili.com/*
+// @include      *://www.bilibili.com/video/*
+// @include      *://www.bilibili.com/medialist*
 // @description  try to take over the world!
 // @author       Liang
-// @match        *://www.bilibili.com/*
+// @match        *://www.bilibili.com/video/*
+// @include      *://www.bilibili.com/medialist*
 // @icon         https://www.bilibili.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
 (function() {
+
+
     var smNum;
+    var a = document.querySelector('#arc_toolbar_report').id;
+
+    if(a){
+
+        for(var i = 1;i <= 10; i += 1){
+
+            setTimeout(()=>{
+                sm2nico();
+            }, i*1000);
 
 
-    setTimeout(()=>{
-        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
-        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
-        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
-        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
-    }, 2000);
+            console.log('执行完毕');
+        }
+    }
 
-    setTimeout(()=>{
-        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
-        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
-        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
-        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
-    }, 3000);
-    
-    setTimeout(()=>{
-        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
-        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
-        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
-        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
-    }, 4000);
-    
-    setTimeout(()=>{
-        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
-        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
-        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
-        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
-    }, 5000);
 
-    setTimeout(()=>{
+    function sm2nico(){
         smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
         document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
         document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
         document.querySelector(".smLink").innerHTML = "跳转" + smNum;
-    }, 6000);
-
-    setTimeout(()=>{
-        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
-        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
-        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
-        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
-    }, 7000);
-
-    setTimeout(()=>{
-        smNum = document.querySelector("a[href*='acg.tv/sm']").innerHTML;
-        document.querySelector("a[href*='acg.tv/sm']").classList.add("smLink");
-        document.querySelector(".smLink").setAttribute("href","https://www.nicovideo.jp/watch/"+smNum);
-        document.querySelector(".smLink").innerHTML = "跳转" + smNum;
-    }, 8000);
+    }
 
 })();
